c4abec54c897edd7a58a06bee6b3102124830db5
Znuxor Znuxor <znuxor@gmail.com>
2017-12-05 00:58:24 -0500

    finished day 5

---
+++ 5b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+data_input = np.loadtxt('5a_data.txt', dtype=int)
+#data_input = np.array([0, 3, 0, 1, -3])
+curr_loc = 0
+step_number = -1
+try:
+    while True:
+        step_number += 1
+        next_jump = data_input[curr_loc]
+        if next_jump < 3:
+            data_input[curr_loc] += 1
+        else:
+            data_input[curr_loc] -= 1
+        curr_loc += next_jump
+except Exception:
+    pass
+finally:
+    print(step_number)
