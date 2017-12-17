34aa17b65b8f3744589b8b674e8da1ae9a8b9cad
Znuxor Znuxor <znuxor@gmail.com>
2017-12-17 01:01:45 -0500

    finished day 17

---
+++ 17a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from collections import deque
+
+puzzle_input = 377
+# puzzle_input = 3
+my_circular_buffer = deque()
+
+my_circular_buffer.append(0)
+
+for i in range(1, 2018):
+    new_pos = puzzle_input % len(my_circular_buffer)
+    my_circular_buffer.rotate(-new_pos-1)
+    my_circular_buffer.appendleft(i)
+my_circular_buffer.rotate(-my_circular_buffer.index(2017))
+print(my_circular_buffer[1])
